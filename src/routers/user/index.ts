b49f4a91cfb31610fceeb7bc47e509172cf33f76
Sharim <p.brejdak@gmail.com>
2019-04-14 05:56:55 +0200

    Added route for UserHistoryRouter

--- src/routers/user/index.ts
+++ src/routers/user/index.ts
@@ -4,11 +4,13 @@
 import * as express from 'express';
 import { Auth } from './auth.router';
 import { UserPersonalsRouter } from './user-personals.router';
+import { UserHistoryRouter } from './user-history.router';
 
 const user = express.Router();
 // normal router
 user.use('/auth', new Auth().getRoutes());
 // CRUD router
 user.use('/', new UserPersonalsRouter().getRoutes());
+user.use('/', new UserHistoryRouter().getRoutes());
 
 export default user;
